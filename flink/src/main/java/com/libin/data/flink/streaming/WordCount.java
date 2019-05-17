23da7954c9fbb0e926c7993f56969c3c9f7ede56
baolibin <2578858653@qq.com>
2019-05-17 21:37:54 +0800

    update

---
+++ flink/src/main/java/com/libin/data/flink/streaming/WordCount.java
@@ -0,0 +1,10 @@
+package com.libin.data.flink.streaming;
+
+/**
+ * Copyright (c) 2019/05/17. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class WordCount {
+}
