71258ce9ef3e1417d8b6297b49c3a38f6657ccad
baolibin <2578858653@qq.com>
2019-05-24 23:07:07 +0800

    update

--- flink/src/main/java/com/libin/data/flink/batch/WordCount.java
+++ flink/src/main/java/com/libin/data/flink/batch/WordCount.java
@@ -4,7 +4,7 @@ package com.libin.data.flink.batch;
  * Copyright (c) 2019/05/17. xixi Inc. All Rights Reserved.
  * Authors: libin <2578858653@qq.com>
  * <p>
- * Purpose :
+ * Purpose : 离线处理
  */
 public class WordCount {
 }
