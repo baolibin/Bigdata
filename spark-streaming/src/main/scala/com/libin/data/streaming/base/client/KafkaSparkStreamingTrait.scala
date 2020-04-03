d8731ceb4890e141d298d8b7fafcd92833536eaf
baolibin <2578858653@qq.com>
2020-04-03 15:20:37 +0800

    update dir

--- spark-streaming/src/main/scala/com/libin/data/streaming/base/client/KafkaSparkStreamingTrait.scala
+++ spark-streaming/src/main/scala/com/libin/data/streaming/base/client/KafkaSparkStreamingTrait.scala
@@ -1,8 +1,8 @@
 package com.libin.data.streaming.base.client
 
 /**
- * Copyright (c) 2020/4/3 XiaoMi Inc. All Rights Reserved.
- * Authors: libin<baolibin@xiaomi.com>
+ * Copyright (c) 2020/4/3 libin Inc. All Rights Reserved.
+ * Authors: libin<2578858653@qq.com>
  *
  * Purpose :
  */
