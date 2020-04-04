9ef49843c9f0303b54e22ea3f91bc3c58d900272
baolibin <2578858653@qq.com>
2020-04-04 22:20:21 +0800

    update

---
+++ spark-streaming/src/main/scala/com/libin/data/streaming/jobs/GenCodeFromCheckpoint.scala
@@ -0,0 +1,11 @@
+package com.libin.data.streaming.jobs
+
+/**
+  * Copyright (c) 2020/04/04. libin Inc. All Rights Reserved.
+  * Authors: libin <2578858653@qq.com>
+  * <p>
+  * Purpose :
+  */
+object GenCodeFromCheckpoint {
+
+}
