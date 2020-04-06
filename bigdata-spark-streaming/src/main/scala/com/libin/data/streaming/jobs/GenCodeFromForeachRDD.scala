d68f12728119d267991e51775bda2b478f28b03a
baolibin <2578858653@qq.com>
2020-04-06 18:31:37 +0800

    foreachRdd

--- bigdata-spark-streaming/src/main/scala/com/libin/data/streaming/jobs/GenCodeFromForeachRDD.scala
+++ bigdata-spark-streaming/src/main/scala/com/libin/data/streaming/jobs/GenCodeFromForeachRDD.scala
@@ -9,15 +9,15 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  * Copyright (c) 2020/4/3 libin Inc. All Rights Reserved.
  * Authors: libin<2578858653@qq.com>
  *
- * Purpose :
+ * Purpose :  nc -lk 9999
  */
 object GenCodeFromForeachRDD {
 	def main(args: Array[String]): Unit = {
-		val conf = new SparkConf().setMaster("local[2]").setAppName("GenCodeFromJoin")
+		val conf = new SparkConf().setMaster("local[2]").setAppName("GenCodeFromForeachRDD")
 		val ssc = new StreamingContext(conf, Seconds(5))
 		
 		StreamingExamples.setStreamingLogLevels()
-		val lines = ssc.socketTextStream("localhost", 9999)
+		val lines = ssc.socketTextStream("areyouok", 9999)
 		
 		lines.map((_, 1))
 			.foreachRDD {
