508442b11160826d985634c4834d6eb7d3028fb5
baolibin <2578858653@qq.com>
2020-04-14 23:47:38 +0800

    add path utils

---
+++ spark-core/src/main/scala/com/libin/utils/PathUtils.scala
@@ -0,0 +1,79 @@
+package com.libin.utils
+
+import java.net.URI
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
+import org.joda.time.DateTime
+
+/**
+ * Copyright (c) 2020/4/14. libin Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose : 
+ */
+object PathUtils {
+
+    /**
+     * 路径拼接DateTime
+     *
+     * @param root 数据根目录
+     * @param date 读取的数据日期
+     * @return 完整数据路径
+     */
+    def pathAssemble(root: String, date: DateTime): String = s"$root/date=${date.toString(DateUtils.DATE_FORMAT)}"
+
+    def pathAssembleAll(root: String, date: DateTime): String = s"$root/date=${date.toString(DateUtils.DATE_FORMAT)}/*"
+
+    /**
+     * 获得Hadoop输入配置
+     */
+    def getHadoopConfig: Configuration = {
+        val hadoopConf = new Configuration()
+        hadoopConf.set("mapreduce.input.fileinputformat.split.maxsize", "512000000")
+        hadoopConf.set("mapreduce.input.fileinputformat.split.minsize", "268435456")
+        hadoopConf
+    }
+
+    /**
+     * 返回目录下所有文件FileStatus信息
+     *
+     * @param conf        Configuration
+     * @param yarnLogPath 指定作业的路径
+     */
+    def getFsStatusArr(conf: Configuration, yarnLogPath: String): Array[FileStatus] = {
+        val fs: FileSystem = FileSystem.get(URI.create(yarnLogPath), conf)
+        fs.listStatus(new Path(yarnLogPath))
+    }
+
+    /**
+     * 检测带分区的数据表，返回指定路径下面有数据最新分区的数据日期.
+     * 可以用于判断天、周、月、年作业，不用关注上游数据表生成周期日期，取最近有数据的分区数据与指定天做对比，判断是否是需要读取的数据。
+     *
+     * @param path 数据父路径
+     * @return
+     */
+    def getLatestDate(path: String): String = {
+        val fsStatus: Array[FileStatus] = getFsStatusArr(getHadoopConfig, path)
+
+        fsStatus
+                .map(_.getPath.toString.trim)
+                // 合格的数据目录
+                .filter(_.contains(SeparatorUtils.splitEqual))
+                .filter {
+                    x =>
+                        // 当前分区数据目录下面存在SUCCESS文件，确保数据已经正常生成完毕
+                        val pathArr = getFsStatusArr(getHadoopConfig, x).map {
+                            x =>
+                                x.getPath.toString
+                        }
+                        pathArr.exists(_.contains(SeparatorUtils.pathSuccess))
+                }
+                // 取出路径中数据分区信息
+                .map(_.split(SeparatorUtils.splitEqual).last)
+                .filter(_.length == 8)
+                // 取最近的一份数据
+                .maxBy(_.toLong)
+    }
+
+}
---
+++ spark-core/src/main/scala/com/libin/utils/SeparatorUtils.scala
@@ -0,0 +1,21 @@
+package com.libin.utils
+
+/**
+ * Copyright (c) 2020/4/14. libin Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose : 
+ */
+object SeparatorUtils {
+    val splitEqual = "=" // 等号字符
+    val splitSpace = " " // 空格字符
+    val splitTable = "\t" // 制表符字符
+    val splitPoint = "\\." // 点字符
+    val splitComma = "," // 逗号分隔字符
+    val splitSemicolon = ";" // 分号字符
+    val splitBar = "-" // 横杠分隔符
+    val splitLeftSlash = "/" // 左斜线
+
+    val pathSuccess = "_SUCCESS" // 数据成功标识符
+
+}
