f1285c214730f5f1a0957106d54f21f386117332
baolibin <2578858653@qq.com>
2020-09-06 09:13:07 +0800

    flink update

--- bigdata-flink/src/main/scala/com/libin/data/flink/base/client/KafkaFlinkStreamingTrait.scala
+++ bigdata-flink/src/main/scala/com/libin/data/flink/base/client/KafkaFlinkStreamingTrait.scala
@@ -4,7 +4,7 @@ package com.libin.data.flink.base.client
  * Copyright (c) 2020/4/3 libin Inc. All Rights Reserved.
  * Authors: libin<2578858653@qq.com>
  *
- * Purpose :
+ * Purpose : Kafka和Flink结合trait
  */
 trait KafkaFlinkStreamingTrait {
 	
---
+++ bigdata-flink/src/main/scala/com/libin/data/flink/streaming/jobs/GenCodeFromMysql.scala
@@ -0,0 +1,11 @@
+package com.libin.data.flink.streaming.jobs
+
+/**
+ * Copyright (c) 2020/9/6. libin Inc. All Rights Reserved.
+ * Authors: libin <libin>
+ * <p>
+ * Purpose : 将处理的数据写入到MySQL中
+ */
+object GenCodeFromMysql {
+
+}
