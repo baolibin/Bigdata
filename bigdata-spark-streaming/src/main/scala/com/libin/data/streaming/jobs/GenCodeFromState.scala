ea2f52cb49490eb9752ca0106d36ddf133653583
baolibin <2578858653@qq.com>
2021-07-05 16:20:47 +0800

    state

---
+++ bigdata-spark-streaming/src/main/scala/com/libin/data/streaming/jobs/GenCodeFromState.scala
@@ -0,0 +1,13 @@
+package com.libin.data.streaming.jobs
+/**
+  * Copyright (c) 2019/02/16. libin Inc. All Rights Reserved.
+  * Authors: libin <2578858653@qq.com>
+  * <p>
+  * Purpose :
+  */
+object GenCodeFromState {
+    def main(args: Array[String]): Unit = {
+
+    }
+
+}
